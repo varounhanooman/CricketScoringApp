cae05dc63b7f3377ca8d4ce4bd5455e043748ab8
SRamlogan <sahil.ak.ramlogan@gmail.com>
2020-10-04 13:20:36 -0400

    included teamlist table and insert and read functions

--- app/src/main/java/com/example/cricket/DataBaseHandler.kt
+++ app/src/main/java/com/example/cricket/DataBaseHandler.kt
@@ -18,9 +18,36 @@ var COL_WICKET_STATUS = "wicketStatus"
 var COL_OVERS_BALL = "oversBall"
 var COL_BALL_LEGAL_EXTRA = "ballLegalExtra"
 
+val HTEAM_TABLE_NAME = "HomeTeamList" 
+var HTEAM_COL_PLAYER_ID = "playerId" /*Unique ID Number for each player 
+we have to create a home and away team list which led to me creating the table below
+this way we can also store less data in this table and we can create a query to get the
+player name to display on both the name list and the main scoring screen or wherever
+also this way we only store an id in the data frame*/
+var HTEAM_COL_FIRST_NAME = "firstName"
+//var HTEAM_COL_LAST_NAME = "lastName"
+var HTEAM_COL_RSCORED = "runsScored" //runs scored when batting
+var HTEAM_COL_NOUT = "notOut" /*whether the batsman is out or not when the team is finished batting. 
+For simplicity we can set this to true (1) for the batsmen not out at the end of the overs as well as
+the ppl who didnt get to bat yet*/ 
+var HTEAM_COL_RCONCEDED = "runsConceded" //The amount of runs the bowler goes for
+var HTEAM_COL_WICKETS = "wickets" //wickets taken by the bowler
+
+/* This is an idea for later on... These would be for historical stats
+val PLAYER_TABLE_NAME = "Players"
+var PLAYER_ID = "playerId"
+var PLAYER_FNAME = "firstName"
+var PLAYER_LNAME = "lastName"
+var PLAYER_MATCHES = "noOfMatches"
+var PLAYER_INNINGS = "noOfInnings"
+var PLAYER_RSCORED = "ttlRunsScored"
+var PLAYER_NOUT = "ttlNotOuts"
+var PLAYER_WICKETS = "ttlWickets"
+var PLAYER_RCONCEDE = "ttlRunsConceded"
+*/
 class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,null,1) {
     override fun onCreate(db: SQLiteDatabase?) {
-        val createTable = "CREATE TABLE " + TABLE_NAME +" (" +
+        val createTable1 = "CREATE TABLE " + TABLE_NAME +" (" +
                 COL_ID +" INTEGER PRIMARY KEY AUTOINCREMENT," +
                 COL_MATCH_ID + " INTEGER," +
                 COL_RUN + " INTEGER," +
@@ -29,7 +56,17 @@ class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context, DATABASE
                 COL_WICKET_STATUS + " VARCHAR(256)," +
                 COL_OVERS_BALL + " FLOAT," +
                 COL_BALL_LEGAL_EXTRA + " VARCHAR(256) )";
-        db?.execSQL(createTable)
+        db?.execSQL(createTable1)
+
+        val createTable2 = "CREATE TABLE " + HTEAM_TABLE_NAME +" (" +
+                HTEAM_COL_PLAYER_ID +" INTEGER PRIMARY KEY AUTOINCREMENT," +
+                HTEAM_COL_FIRST_NAME + " VARCHAR(256)," +
+                //HTEAM_COL_LAST_NAME + " VARCHAR(256)," +
+                HTEAM_COL_RSCORED + " INTEGER," +
+                HTEAM_COL_NOUT + " INTEGER," +
+                HTEAM_COL_RCONCEDED + " INTEGER," +
+                HTEAM_COL_WICKETS + " INTEGER )";
+        db?.execSQL(createTable2)
 
 
     }
@@ -37,7 +74,7 @@ class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context, DATABASE
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
     }
-
+/*******************Ball Data Frame Handler***************************************************/
     fun insertData(ballDataFrame : BallDataFrame){
         val db = this.writableDatabase
         var cv = ContentValues()
@@ -98,7 +135,51 @@ class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context, DATABASE
     fun deleteData(){
         val db = this.writableDatabase
 
-        db.delete(TABLE_NAME, null,null)
+        db.delete(TABLE_NAME, null, null)
+        db.delete(HTEAM_TABLE_NAME, null, null)
+    }
+
+    /***************Team List Handler*******************************************************/
+    fun insertDataTeamList(homeTeamList : HomeTeamList){
+            val db = this.writeableDatabase
+            var cv = ContentValues()
+            cv.put(HTEAM_COL_PLAYER_ID, homeTeamList.playerId)
+            cv.put(HTEAM_COL_FIRST_NAME, homeTeamList.firstName)
+            //cv.put(HTEAM_COL_LAST_NAME, homeTeamList.lastName)
+            cv.put(HTEAM_COL_RSCORED, homeTeamList.runsScored)
+            cv.put(HTEAM_COL_NOUT, homeTeamList.notOut)
+            cv.put(HTEAM_COL_RCONCEDED, homeTeamList.runsConceded)
+            cv.put(HTEAM_COL_WICKETS, homeTeamList.wickets)
+            try {
+                var newHomePlayer = db.insert(HTEAM_TABLE_NAME, null, cv)
+                Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
+            }
+            catch(Exception e) {
+                Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+            }
+    }
+
+    fun readDataTeamList() : MutableList<HomeTeamList>{
+        var list : MutableList<HomeTeamList> = ArrayList()
+        val db = this.readableDatabase
+        val query = "Select * from " + HTEAM_TABLE_NAME
+        val result = db.rawQuery(query,null)
+        if (result.moveToFirst())
+            do {
+                var homeTeamList = HomeTeamList()
+                homeTeamList.playerId = result.getString(result.getColumnIndex(HTEAM_COL_PLAYER_ID)).toInt()
+                homeTeamList.firstName = result.getString(result.getColumnIndex(HTEAM_COL_FIRST_NAME))
+                //homeTeamList.lastName = result.getString(result.getColumnIndex(HTEAM_COL_LAST_NAME))
+                homeTeamList.runsScored = result.getString(result.getColumnIndex(HTEAM_COL_RSCORED)).toInt()
+                homeTeamList.notOut = result.getString(result.getColumnIndex(HTEAM_COL_NOUT)).toInt()
+                homeTeamList.runsConceded = result.getString(result.getColumnIndex(HTEAM_COL_RCONCEDED)).toInt()
+                homeTeamList.wickets = result.getString(result.getColumnIndex(HTEAM_COL_WICKETS)).toInt()
+                list.add(homeTeamList)
+            } while (result.moveToNext())
+
+        result.close()
+        db.close()
+        return list
     }
 
 }
---
+++ app/src/main/java/com/example/cricket/HomeTeamList.kt
@@ -0,0 +1,28 @@
+package com.example.cricket
+
+class HomeTeamList{
+
+    var playerId: Int = 0
+    var firstName: String = ""
+    //var lastName: String = ""
+    var runsScored: Int = 0
+    var notOut: Int = 0
+    var runsConceded: Int = 0
+    var wickets: Int = 0
+
+    constructor(playerId:Int, firstName:String,/*lastName:String,*/runsScored:Int,notOut:Int,runsConceded:Int,wickets:Int){
+        this.playerId = playerId
+        this.firstName = firstName
+        //this.lastName = lastName
+        this.runsScored = runsScored
+        this.notOut = notOut
+        this.runsConceded = runsConceded
+        this.wickets = wickets
+    }
+
+    constructor(){
+    }
+
+}
+
+
